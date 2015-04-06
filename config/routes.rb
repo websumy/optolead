ab5c988ff72cbb2972124d9cafdbd314367a6740
Andrey Konoplenko <insumy@gmail.com>
2015-04-06 20:07:14 +0300

    remove registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :users
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
   root 'leads#index'
 
   namespace :bitrix do
